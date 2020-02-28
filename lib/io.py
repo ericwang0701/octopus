f18e20bb0e21af0a4cc446ad85d0e6709d94615f
Eric Wang <ericwang0701@gmail.com>
2020-02-27 22:50:14 -0500

    add write_mesh2 for obj format

--- lib/io.py
+++ lib/io.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import json
 import numpy as np
@@ -55,3 +56,38 @@ def write_mesh(filename, v, f):
     with open(filename, 'w') as fp:
         fp.write(('v {:f} {:f} {:f}\n' * len(v)).format(*v.reshape(-1)))
         fp.write(('f {:d} {:d} {:d}\n' * len(f)).format(*(f.reshape(-1) + 1)))
+
+
+def write_mesh2(filename, v, f, vt=None, ft=None, vn=None, vc=None, texture=None):
+    with open(filename, 'w') as fp:
+        if texture is not None:
+            mat_file = filename.replace('obj', 'mtl')
+
+            fp.write('mtllib {}\n'.format(os.path.basename(mat_file)))
+            fp.write('usemtl mat\n')
+
+            with open(mat_file, 'w') as mfp:
+                mfp.write('newmtl mat\n')
+                mfp.write('Ka 1.0 1.0 1.0\n')
+                mfp.write('Kd 1.0 1.0 1.0\n')
+                mfp.write('Ks 0.0 0.0 0.0\n')
+                mfp.write('d 1.0\n')
+                mfp.write('Ns 0.0\n')
+                mfp.write('illum 0\n')
+                mfp.write('map_Kd {}\n'.format(texture))
+
+        if vc is not None:
+            fp.write(('v {:f} {:f} {:f} {:f} {:f} {:f}\n' * len(v)).format(*np.hstack((v, vc)).reshape(-1)))
+        else:
+            fp.write(('v {:f} {:f} {:f}\n' * len(v)).format(*v.reshape(-1)))
+
+        if vn is not None:
+            fp.write(('vn {:f} {:f} {:f}\n' * len(vn)).format(*vn.reshape(-1)))
+
+        if vt is not None:
+            fp.write(('vt {:f} {:f}\n' * len(vt)).format(*vt.reshape(-1)))
+
+        if ft is not None:
+            fp.write(('f {:d}/{:d}/{:d} {:d}/{:d}/{:d} {:d}/{:d}/{:d}\n' * len(f)).format(*np.hstack((f.reshape(-1, 1), ft.reshape(-1, 1), f.reshape(-1, 1))).reshape(-1) + 1))
+        else:
+            fp.write(('f {:d}//{:d} {:d}//{:d} {:d}//{:d}\n' * len(f)).format(*np.repeat(f.reshape(-1) + 1, 2)))
